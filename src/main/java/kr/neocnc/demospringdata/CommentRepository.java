cc03302baa21fc2fbc3bc3b544e01dc4b8874120
namkyu bae <baenamkyu@gmail.com>
2019-05-30 00:54:50 +0900

    17.Repository 인터페이스 정의하기

---
+++ src/main/java/kr/neocnc/demospringdata/CommentRepository.java
@@ -0,0 +1,8 @@
+package kr.neocnc.demospringdata;
+
+import org.springframework.data.repository.RepositoryDefinition;
+
+import java.util.List;
+
+public interface CommentRepository extends MyRepository<Comment, Long> {
+}
