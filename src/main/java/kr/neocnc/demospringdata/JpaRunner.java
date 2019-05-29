f71cfb42a82ac0b68e2fc738a097e2923fc2eb15
namkyu bae <baenamkyu@gmail.com>
2019-05-30 00:29:16 +0900

    16.리포지토리

--- src/main/java/kr/neocnc/demospringdata/JpaRunner.java
+++ src/main/java/kr/neocnc/demospringdata/JpaRunner.java
@@ -1,11 +1,9 @@
 package kr.neocnc.demospringdata;
 
-import org.hibernate.Session;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
@@ -14,15 +12,16 @@ import javax.transaction.Transactional;
 public class JpaRunner implements ApplicationRunner {
 
     @PersistenceContext
-    EntityManager entityManager;
+    PostRepository postRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Account account = new Account();
-        account.setUsername("baenamkyu");
-        account.setPassword("hibernate");
+        Post post = new Post();
+        post.setTitle("spring");
 
-        Session session = entityManager.unwrap(Session.class);
-        session.save(account);
+        Comment comment = new Comment();
+        comment.setComment("hello");
+
+        postRepository.save(post);
     }
 }
--- src/main/java/kr/neocnc/demospringdata/User.java
+++ src/main/java/kr/neocnc/demospringdata/User.java
@@ -1,14 +1,14 @@
 package kr.neocnc.demospringdata;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
-public class Account {
+public class User {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
 
     private String username;
