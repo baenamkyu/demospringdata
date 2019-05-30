9b02a5731ebb9006d3978da0deade97b11771397
baenamkyu <baenamkyu@gmail.com>
2019-05-30 16:03:16 +0900

    21

--- src/main/java/kr/neocnc/demospringdata/CommentRepository.java
+++ src/main/java/kr/neocnc/demospringdata/CommentRepository.java
@@ -1,8 +1,16 @@
 package kr.neocnc.demospringdata;
 
-import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
 public interface CommentRepository extends MyRepository<Comment, Long> {
+
+    @Async
+    Future<List<Comment>> findByCommentContainsIgnoreCase(String keyword, Pageable pageable);
+
 }
--- src/test/java/kr/neocnc/demospringdata/CommentRepositoryTest.java
+++ src/test/java/kr/neocnc/demospringdata/CommentRepositoryTest.java
@@ -4,9 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,15 +24,25 @@ public class CommentRepositoryTest {
     CommentRepository commentRepository;
 
     @Test
-    public void crud() {
-        Comment comment = new Comment();
-        comment.setComment("Hello Comment");
-        commentRepository.save(comment);
+    public void crud() throws ExecutionException, InterruptedException {
+        this.createComment(100, "spring data jpa");
+        this.createComment(55, "hibernate SPRING");
 
-        List<Comment> all = commentRepository.findAll();
-        assertThat(all.size()).isEqualTo(1);
+        PageRequest pageRequest = PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "likeCount"));
 
-        long count = commentRepository.count();
-        assertThat(count).isEqualTo(1);
+        Future<List<Comment>> future =
+                commentRepository.findByCommentContainsIgnoreCase("Spring", pageRequest);
+        System.out.println("==================");
+        System.out.println("is done?" + future.isDone());
+
+        List<Comment> comments = future.get();
+        comments.forEach(System.out::println);
+    }
+
+    private void createComment(int likeCount, String comment) {
+        Comment newComment = new Comment();
+        newComment.setLikeCount(likeCount);
+        newComment.setComment(comment);
+        commentRepository.save(newComment);
     }
 }
